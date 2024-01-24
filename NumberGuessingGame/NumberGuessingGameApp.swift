3e2b677b21e16bf92aa616e5072edc3600378d9a
6410615030 <6410615030@student.tu.ac.th>
2024-01-24 21:02:47 +0700

    Initial Commit

---
+++ NumberGuessingGame/NumberGuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberGuessingGameApp.swift
+//  NumberGuessingGame
+//
+//  Created by Babypowder on 24/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct NumberGuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
