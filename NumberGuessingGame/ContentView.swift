f618f488f11cf1eed6d0fecec744924c4973f002
6410615030 <6410615030@student.tu.ac.th>
2024-01-29 01:01:19 +0700

    29.1.24

--- NumberGuessingGame/ContentView.swift
+++ NumberGuessingGame/ContentView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var game = Game()
+    @State var guessNum: Double = 0.0
+    @State var showResult = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("GUESS THE NUMBER")
+            
+                Text(String(Game.value(num: guessNum)))
+                HStack {
+                    Text("0")
+                    Slider(value: $guessNum)
+                    Text("100")
+                }
+                
+            Button("Check") {
+                showResult = true
+                game.check(guess: guessNum)
+            }
+
+            .alert(isPresented: $showResult) {
+                Alert(
+                    title: Text("Result"),
+                    message: Text(String(game.msg)),
+                    dismissButton: .default(Text("OK")) {
+                        if game.status {
+                            guessNum = 0.0
+                            game.startNewGame()
+                        }
+
+                    }
+                    
+                )
+            
+            
+            }.padding()
+            Text("You have tried \(game.n) times")
+
         }
         .padding()
     }
---
+++ NumberGuessingGame/Model/Game.swift
@@ -0,0 +1,40 @@
+//
+//  Game.swift
+//  NumberGuessingGame
+//
+//  Created by Babypowder on 24/1/2567 BE.
+//
+
+import Foundation
+struct Game {
+    var target = Double.random(in: 0...1)
+    var n = 0
+    var status = false
+    var msg = ""
+    mutating func check(guess: Double) {
+        let guessNum = lround(guess*100)
+        let check = lround(target*100)
+        if guessNum == check {
+            msg = "You won in \(n) tries!"
+            status = true
+        } else if guessNum < check {
+            msg = "Greater than \(guessNum) !!"
+            n += 1
+            status = false
+        } else {
+            msg = "Less than \(guessNum) !!"
+            n += 1
+            status = false
+        }
+    return
+    }
+    
+    mutating func startNewGame() {
+        n = 0
+        target = Double.random(in: 0...1)
+    }
+
+    static func value(num: Double) -> Int{
+        return lround(num*100)
+    }
+}
